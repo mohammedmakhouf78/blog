e094207b2b5504a5aed5a7bcca2016d7a4198dec
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2026-02-08 12:28:11 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import vitePluginSvgr from "vite-plugin-svgr";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss(), vitePluginSvgr({})],
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [react(), sitemap()],
+
+  markdown: {
+    shikiConfig: {
+      defaultColor: false,
+      themes: {
+        light: "github-light-high-contrast", // one-light
+        dark: "github-dark", // plastic
+      },
+      wrap: true,
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+
+  prefetch: {
+    prefetchAll: true,
+    // defaultStrategy: "load",
+  },
+
+  output: "static",
+  site: "https://ryze.pages.dev",
+});
